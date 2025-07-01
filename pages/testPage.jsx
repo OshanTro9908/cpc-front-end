7c8cb45d9f5be54d3d6a4c881d87a7fb1ac3db2f
Oshan Tharuka <oshantharuka246@gmail.com>
2025-07-01 15:36:02 +0530

    test page - use state hook function

--- pages/testPage.jsx
+++ pages/testPage.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 export default function TestPage() {
+    const[count, setCount] = useState(0);//hock is geven two type things count - variable setcount - function
+     
+    function add(){
+        setCount(count + 1);
+    }
+
+    function sub(){
+        setCount(count - 1);
+    }
+
     return (
          <div className="w-full h-screen bg-black flex justify-center items-center">
-            <h1>test page</h1>
+            <div className="w-[400px] h-[400px] bg-white flex flex-col justify-center items-center rounded-2xl">
+            <h1 className="text-5xl font-bold">{count}</h1>
+            <div className="w-full flex justify-center items-center  h-[100px]">
+            <button onClick={add} className = "w-[100px] bg-blue-500 h-[45px] text-3xl mx-2 flex justify-center items-center text-white rounded-full" >+</button>
+            <button onClick={sub} className = "w-[100px] bg-blue-500 h-[45px] text-3xl mx-2 flex justify-center items-center text-white rounded-full" >-</button>
+            </div>
           
         </div>
-    );
+        </div>
+    ); 
 }
